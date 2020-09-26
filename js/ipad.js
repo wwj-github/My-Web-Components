01580479ff1ce2470ed24387227c0548e7d0e00c
wwj-github <wwjdoodlemail@163.com>
2020-09-26 12:39:14 +0800

    0.0.1

--- js/ipad.js
+++ js/ipad.js
@@ -8,20 +8,76 @@ function Ide(props) {
         width: '90vw',
         height: '90vh',
         backgroundColor: 'lightgray',
+        fontSize: `${props.fontsize}rem`,
     };
-    const tpl = (React.createElement("textarea", { style: style }));
+    const tpl = (React.createElement("textarea", { style: style, spellCheck: "false" }));
+    return tpl;
+}
+function Panel(props) {
+    const style = {
+        margin: '20px 0 20px 0',
+        border: '1px solid gray',
+    };
+    const btn_style = {
+        margin: '0 20px 0 20px',
+        fontSize: `${props.fontsize}rem`,
+    };
+    const btn_style2 = {
+        margin: '0 20px 0 20px',
+        backgroundColor: 'lightgray',
+        fontSize: `${props.fontsize}rem`,
+    };
+    const label_style = {
+        border: '1px solid black',
+    };
+    const tpl = (React.createElement("div", { style: style },
+        React.createElement("button", { onClick: clear, style: btn_style }, "Clear"),
+        React.createElement("button", { onClick: () => { props.callback(0.2); }, style: btn_style2 }, "Font+"),
+        React.createElement("button", { onClick: () => { props.callback(-0.2); }, style: btn_style2 }, "Font-"),
+        React.createElement("button", { onClick: run, style: btn_style }, "Run"),
+        React.createElement("label", { style: label_style }, "Return Value:")));
+    return tpl;
+}
+function App(props) {
+    const [fontSize, setFontSize] = React.useState(1);
+    const changFontSize = (x) => {
+        setFontSize(fontSize + x);
+    };
+    const tpl = (React.createElement("div", null,
+        React.createElement(Panel, { callback: changFontSize, fontsize: fontSize }),
+        React.createElement(Ide, { fontsize: fontSize })));
     return tpl;
 }
 window.onload = () => {
-    const tpl = (React.createElement("div", null,
-        React.createElement("button", { onClick: run }, "Run"),
-        React.createElement("br", null),
-        React.createElement(Ide, null)));
-    ReactDOM.render(tpl, document.querySelector('#app'));
+    ReactDOM.render(React.createElement(App, null), document.querySelector('#app'));
 };
 function run() {
     let ta = q('textarea');
-    let cd = ta === null || ta === void 0 ? void 0 : ta.value;
-    let f = new Function(cd);
-    f();
+    let lab = q('label');
+    if (lab === null) {
+        return;
+    }
+    if (ta === null) {
+        return;
+    }
+    let cd2 = `return (()=>${ta.value})()`;
+    let cd = ta.value;
+    let f = new Function(cd2);
+    // let f2=new Function(cd2)
+    try {
+        let r = f();
+        // if(r===undefined){
+        //     r=f2()
+        // }
+        lab.innerText = r;
+    }
+    catch (e) {
+        lab.innerText = e;
+    }
+}
+function clear() {
+    let ta = q('textarea');
+    if (ta !== null) {
+        ta.value = '';
+    }
 }
--- ts/ipad.tsx
+++ ts/ipad.tsx
@@ -9,30 +9,104 @@ function Ide(props:any){
         width:'90vw',
         height:'90vh',
         backgroundColor:'lightgray',
+        fontSize:`${props.fontsize}rem`,
 
     }
     const tpl = (
-        <textarea style={style}></textarea>
+        <textarea style={style} spellCheck="false"></textarea>
     );
 
     return tpl;
 }
 
+function Panel(props:any){
+    const style = {
+        margin:'20px 0 20px 0',
+        border: '1px solid gray',
+    };
 
-window.onload = ()=>{
+    const btn_style = {        
+        margin:'0 20px 0 20px',
+        fontSize:`${props.fontsize}rem`,
+    };
+
+    const btn_style2 = {       
+        margin:'0 20px 0 20px', 
+        backgroundColor:'lightgray',
+        fontSize:`${props.fontsize}rem`,
+    };
+
+    const label_style = {
+        border:'1px solid black',
+        // minWidth:'100px',
+    }
+    const tpl = (
+        <div style={style}>
+            {/* 与 Run 分开, 防止误触 */}
+            <button onClick={clear} style={btn_style}>Clear</button>
+            
+            <button onClick={()=>{props.callback(0.2)}} style={btn_style2}>Font+</button>            
+            <button onClick={()=>{props.callback(-0.2)}} style={btn_style2}>Font-</button>            
+                        
+            <button onClick={run} style={btn_style}>Run</button>
+            
+            <label style={label_style}>Return Value:</label>
+        </div>
+    )
+
+    return tpl;
+}
+
+function App(props:any){
+    
+    const [fontSize, setFontSize] = React.useState(1);
+
+    const changFontSize = (x:number)=>{
+        setFontSize(fontSize+x)
+    }
     const tpl = (
         <div>
-            <button onClick={run}>Run</button><br/>
-            <Ide/>
+            <Panel callback={changFontSize} fontsize={fontSize}/>   
+            <Ide fontsize={fontSize}/>
         </div>        
     )
-    ReactDOM.render(tpl, document.querySelector('#app')); 
+
+    return tpl;
+}
+
+window.onload = ()=>{
+    ReactDOM.render(<App/>, document.querySelector('#app')); 
 }
 
 function run(){
     let ta=q('textarea')
-    let cd:any=ta?.value
-    let f=new Function(cd)
-    f()
+    let lab=q('label')
+    if(lab === null){
+        return;
+    }
+    if(ta === null){
+        return;
+    }
+
+    let cd2:any = `return (()=>${ta.value})()`;
+    let cd:any = ta.value;
+    let f=new Function(cd2)
+    // let f2=new Function(cd2)
+    try{
+        let r=f()
+        // if(r===undefined){
+        //     r=f2()
+        // }
+        lab.innerText = r
+    }catch(e){        
+        lab.innerText = e
+    }
+    
 }
 
+function clear(){
+    let ta=q('textarea')
+    if(ta !== null){
+        ta.value = ''
+    }
+}
